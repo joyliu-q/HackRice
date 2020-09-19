567070ebec0102f1a5ea0f72d3bc9cd62eeb13ec
Jacario777 <jkd7@rice.edu>
2020-09-19 16:22:56 -0500

    changed faq page

--- about.js
+++ about.js
@@ -1,21 +1,7 @@
 Vue.component('faq', {
     data: function () {
       return {
-        // Later extract from JSON
-        links: [
-            {
-                question: "How do you recommend users about social issues?",
-                answer: "We will add a feature to allow members to create an account and select areas and topics they may be interested in, and use these data to recommend our users social issues."
-            },
-            {
-                question: "How do you add new cards?",
-                answer: "We will add a feature to allow organizations to register and create new cards."
-            },
-            {
-                question:"How will you make sure all the cards are introducing social issues, rather than fake news or propaganda?",
-                answer: "We will have a committee to approve every single new card submissions from organizations. We will also check the organization’s previous records to ensure they are using our website appropriately."
-            }
-        ]
+        
       }
     },
     template: `
@@ -23,14 +9,16 @@ Vue.component('faq', {
         <div class = "content">
             <h1>FAQ:</h1>
             <div class = "faq_box">
-                <ul>
-                    <li v-for="link in links">{{link.question}} - {{link.answer}}</li>
-                </ul>
+                <h2>"How do you recommend users about social issues?"</h2>
+                <p>"We will add a feature to allow members to create an account and select areas and topics they may be interested in, and use these data to recommend our users social issues."</p>
             </div>
             <div class = "faq_box">
+                <h2>"How do you add new cards?"</h2>
+                <p>"We will add a feature to allow organizations to register and create new cards."</p>
             </div>
             <div class = "faq_box">
-            </div>
+                <h2>"How will you make sure all the cards are introducing social issues, rather than fake news or propaganda?"</h2>
+                <p>"We will have a committee to approve every single new card submissions from organizations. We will also check the organization’s previous records to ensure they are using our website appropriately."</p>
             </div>
         </div>
     </div>
