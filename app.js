2fb7ae2012be180ce81a71f6b02e5360cc0d7ef5
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-19 10:44:15 -0700

    mustache tests

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 Vue.component('card-overlay', {
     data: function () {
       return {
-        hidecards: true
+        hidecards: true,
+        // Later extract from JSON
+        title: "Detailed Explanation",
+        explanation: "Wow more detailed explanation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nisl arcu, sit amet efficitur est pellentesque nec. Nam vehicula nisi risus, vel tempus quam vehicula a. Aenean a convallis orci. Quisque pharetra orci et elit aliquam commodo.\nProin viverra ante quis turpis malesuada, eu egestas diam sodales. Fusce maximus turpis vel sem egestas dignissim. Aenean mattis ligula mi, eu aliquam velit pulvinar mollis. Nulla placerat ultricies purus, sit amet  maximus nisl vehicula quis. Duis a scelerisque mauris, mattis gravida nisi. Nam sagittis id ipsum ut pretium. Phasellus in  sapien sollicitudin, vehicula metus sit amet, scelerisque ante.   Nulla luctus blandit porta.",
+        links: ["link1", "link2", "link3"]
       }
     },
     template: `
     <div>
         <div id = "overlay_box" v-if="hidecards == false">
-            <h3>Detailed Explanation</h3>
+            <h3>{{title}}</h3>
             <p>
-                Wow more detailed explanation. Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit. Mauris feugiat nisl arcu, sit amet 
-                efficitur est pellentesque nec. Nam vehicula nisi risus, vel 
-                tempus quam vehicula a. Aenean a convallis orci. Quisque pharetra
-                orci et elit aliquam commodo. 
-                
-                Proin viverra ante quis turpis
-                malesuada, eu egestas diam sodales. Fusce maximus turpis vel sem
-                egestas dignissim. Aenean mattis ligula mi, eu aliquam velit
-                pulvinar mollis. Nulla placerat ultricies purus, sit amet
-                maximus nisl vehicula quis. Duis a scelerisque mauris, mattis
-                gravida nisi. Nam sagittis id ipsum ut pretium. Phasellus in 
-                sapien sollicitudin, vehicula metus sit amet, scelerisque ante. 
-                Nulla luctus blandit porta.
+                {{explanation}}
             <p>
             <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZMbzsJS6Lj0Fn1aFVybj3neSLNrY4Wmw1Pg&usqp=CAU">
             <ul>
