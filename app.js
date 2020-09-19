d2b89992d06077559a0ea25ac5b27130a83dce73
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-19 10:08:14 -0700

    overlay successful

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 Vue.component('card-overlay', {
     data: function () {
       return {
@@ -6,29 +5,42 @@ Vue.component('card-overlay', {
       }
     },
     template: `
-    <div class="blog-post" v-if="hidecards == false">
-        <h3>Detailed Explanation</h3>
-        <p>
-            Wow more detailed explanation. Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit. Mauris feugiat nisl arcu, sit amet 
-            efficitur est pellentesque nec. Nam vehicula nisi risus, vel 
-            tempus quam vehicula a. Aenean a convallis orci. Quisque pharetra
-             orci et elit aliquam commodo. 
-             
-             Proin viverra ante quis turpis
-            malesuada, eu egestas diam sodales. Fusce maximus turpis vel sem
-            egestas dignissim. Aenean mattis ligula mi, eu aliquam velit
-            pulvinar mollis. Nulla placerat ultricies purus, sit amet
-             maximus nisl vehicula quis. Duis a scelerisque mauris, mattis
-             gravida nisi. Nam sagittis id ipsum ut pretium. Phasellus in 
-             sapien sollicitudin, vehicula metus sit amet, scelerisque ante. 
-             Nulla luctus blandit porta.
-        <p>
-        <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZMbzsJS6Lj0Fn1aFVybj3neSLNrY4Wmw1Pg&usqp=CAU">
-        <ul>
-            <li>Link</li>
-        </ul>
-        <button v-on:click="hidecards = true">Back to Cards {{ hidecards }}</button>
+    <div>
+        <div class = "overlay_container" v-if="hidecards == false">
+            <h3>Detailed Explanation</h3>
+            <p>
+                Wow more detailed explanation. Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. Mauris feugiat nisl arcu, sit amet 
+                efficitur est pellentesque nec. Nam vehicula nisi risus, vel 
+                tempus quam vehicula a. Aenean a convallis orci. Quisque pharetra
+                orci et elit aliquam commodo. 
+                
+                Proin viverra ante quis turpis
+                malesuada, eu egestas diam sodales. Fusce maximus turpis vel sem
+                egestas dignissim. Aenean mattis ligula mi, eu aliquam velit
+                pulvinar mollis. Nulla placerat ultricies purus, sit amet
+                maximus nisl vehicula quis. Duis a scelerisque mauris, mattis
+                gravida nisi. Nam sagittis id ipsum ut pretium. Phasellus in 
+                sapien sollicitudin, vehicula metus sit amet, scelerisque ante. 
+                Nulla luctus blandit porta.
+            <p>
+            <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZMbzsJS6Lj0Fn1aFVybj3neSLNrY4Wmw1Pg&usqp=CAU">
+            <ul>
+                <li>Link</li>
+            </ul>
+            <button v-on:click="hidecards = true">Back to Cards {{ hidecards }}</button>
+        </div>
+        <div class = "card" v-if="hidecards == true">
+            <h2>Social Justice Issue Title</h2>
+            <img src = "blah.png">
+            <p>
+                Blurb summarizing what the issue is. Maybe add photo.
+            </p>
+            <div id = "button_container">
+                <button class="no_button">X</button>
+                <button class="yes_button">Y</button>
+            </div>
+        </div>
     </div>
     `
 })
