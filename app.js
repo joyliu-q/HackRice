ee770ce483576e41c84a5947fdf58b3096792b49
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-19 11:51:49 -0700

    fixed links

--- app.js
+++ app.js
@@ -2,31 +2,36 @@ Vue.component('card-overlay', {
     data: function () {
       return {
         hidecards: true,
+        email_list_input: "",
         // Later extract from JSON
         title: "Detailed Explanation",
         explanation: "Wow more detailed explanation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nisl arcu, sit amet efficitur est pellentesque nec. Nam vehicula nisi risus, vel tempus quam vehicula a. Aenean a convallis orci. Quisque pharetra orci et elit aliquam commodo.\nProin viverra ante quis turpis malesuada, eu egestas diam sodales. Fusce maximus turpis vel sem egestas dignissim. Aenean mattis ligula mi, eu aliquam velit pulvinar mollis. Nulla placerat ultricies purus, sit amet  maximus nisl vehicula quis. Duis a scelerisque mauris, mattis gravida nisi. Nam sagittis id ipsum ut pretium. Phasellus in  sapien sollicitudin, vehicula metus sit amet, scelerisque ante.   Nulla luctus blandit porta.",
         links: [
             {
                 title: "thirst project",
-                url: "thirstproject.org"
+                url: "https://www.thirstproject.org/"
             },
             {
                 title: "UNICEF",
-                url: "unicef.org"
+                url: "https://www.unicef.org/"
             }
         ]
-      }
+        }
     },
     template: `
     <div>
         <div id = "overlay_box" v-if="hidecards == false">
-            <h3>{{title}}</h3>
+            <h3>{{ title.toLocaleUpperCase() }}</h3>
             <p>
-                {{explanation}}
-            <p>
+                {{ explanation }}
+            </p>
             <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZMbzsJS6Lj0Fn1aFVybj3neSLNrY4Wmw1Pg&usqp=CAU">
             <ul>
-                <li v-for="link in links">{{link.title}} - {{link.url}}</li>
+                <li v-for="link in links">
+                    {{link.title}} - 
+                    <a :href = "link.url">{{link.url}}</a> 
+                    div :id="val"
+                </li>
             </ul>
             <button class = "overlay_back_button" v-on:click="hidecards = true">Back to Cards</button>
         </div>
@@ -41,6 +46,7 @@ Vue.component('card-overlay', {
                 <button class="yes_button" v-on:click="hidecards = false">Y</button>
             </div>
         </div>
+        <input v-model = "email_list_input" type = "text" placeholder = "Join Our Email List For More Info!">
     </div>
     `
 })
